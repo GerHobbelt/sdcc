3c5c345a746160985cd3ca72636e7d5e40fef914
Xuran Cai <xurancai@dmf584.resnet.ust.hk>
2024-03-17 17:17:28 +0800

    update checking if loop end

--- src/SDCCcfg.hpp
+++ src/SDCCcfg.hpp
@@ -27,8 +27,8 @@ structure merge_node{
 
 stucture ps_cfg_t{
 
-  cfg_node begin; //s node
-  cfg_node end; //t node
+  cfg_node &begin; //s node
+  cfg_node &end; //t node
   cfg_t &cfg; //graph contains s and t node
   merge_node &merge; //merge type
 }
@@ -76,12 +76,90 @@ static void loop_merge(ps_cfg_t &cfg_1, ps_cfg_t &cfg_2, ps_cfg_t &cfg_3){
   cfg_3.merge = merge_node_init(3, cfg_1, cfg_2);
 }
 
+static int if_loop_end(cfg_t &cfg, vertex vi){
+  if(indegree(vi,cfg)!=1){
+    return 0;
+  }
+  typename GraphTraits::in_edge_iterator in_i, in_end;
+  boost::tie(in_i, in_end) = in_edges(vi, cfg);
+  Vertex src = source(*in_i, cfg);
+  if(indegree(src,cfg)!=1){
+    return 0;
+  }
+  boost::tie(in_i, in_end) = in_edges(source, cfg);
+  Vertex src = source(*in_i, cfg);
+  if(indegree(src,cfg)==2){
+     for ( boost::tie(in_i, in_end) = in_edges(src, cfg);in_i != in_end; ++in_i) {
+          e = *in_i;
+          Vertex src = source(e, cfg);
+          if(src.index<vi.index){
+            flag++;
+          }
+      }
+      if (flag==1){
+        return 1;
+      }
+  }
+  return 0;
+}
+
+static ps_cfg_t get_parallel_part(cfg_t &cfg, vertex begin_loop){
+  graph_traits<Graph>::vertex_iterator vi, vi_end, next;
+  boost::tie(vi, vi_end) = vertices(cfg);
+  vi=begin_loop;
+  for (next = vi; vi != vi_end; vi = next) {
+      ++next;
+      flag=0;
+      if(indegree(vi,cfg)=2 && out_degree(vi, cfg) == 1){
+        typename GraphTraits::in_edge_iterator in_i, in_end;
+        for ( boost::tie(in_i, in_end) = in_edges(vi, cfg);in_i != in_end; ++in_i) {
+          e = *in_i;
+          Vertex src = source(e, cfg);
+          if(src.index<vi.index){
+            flag++;
+          }
+        }
+        if (flag==2){
+          //this is the end of loop, create the tree  
+       }
+      }
+      if(indegree(vi,cfg)=1 && out_degree(vi, cfg) == 2){
+        //check if it is the end of loop or the start of parallel
+       }
+}
 
 
-
-static void convert_cfg_to_spcfg(cfg_t &cfg, mergelist mlist){
-  //go through the cfg and find the in edge and out edge
-  //if the in edge is 1 and the out edge is 1, then it is just series
-  //if the in edge is 2 and the out edge is 1, then it is the start of loop merge
-  //if the in edge is 1 and the out edge is 2, then it is the start of parallel merge
+static pscfg convert_cfg_to_spcfg(cfg_t &cfg){
+  //convert the original cfg to the root node of ps_cfg
+  ps_cfg_t pscfg;
+  pscfg.cfg=cfg;
+  graph_traits<Graph>::vertex_iterator vi, vi_end, next;
+  boost::tie(vi, vi_end) = vertices(cfg);
+  if (*vi == *(vi_end-1)) {
+    ps_cfg.begin=*vi;
+    ps_cfg.end=vi;
+    ps_cfg.merge=NULL; 
+    return ps_cfg;
+  }
+  pscfg.begin = *vi;
+  pscfg.end = *(vi_end-1);
+  //series merge - indegree=1, outdegree=1
+  if (indegree(vi,cfg)=0 && out_degree(vi, cfg) == 1 && indegree(next,cfg)=1 ){
+    cfg_t cfg_1, cfg_2;
+    boost::copy_graph(cfg, cfg_1, boost::vertex_copy(forget_properties()).edge_copy(forget_properties()));
+    boost::copy_graph(cfg, cfg_2, boost::vertex_copy(forget_properties()).edge_copy(forget_properties()));
+    remove_vertex(*vi, cfg_2);
+    vi=next;
+    for (next = vi; vi != vi_end; vi = next) {
+      ++next;
+      remove_vertex(*vi, cfg_1);
+   }
+   pscfg.merge = merge_node_init(1, convert_cfg_to_spcfg(cfg_1),convert_cfg_to_spcfg(cfg_2) );
+  }
+  //start of parallel merge - in degree=1, out degree=2, both next verdexes with larger index
+  //end of parallel merge - in degree=2, out degree=1, both previous vertexes with smaller index
+  //start of loop merge - in degree=2, out degree=1, one previous vertex is larger and one is smaller
+  //end of loop merge - in degree=1, out degree=2, both next vertexes with larger index.
+  //index of end of loop is always 2 larger than loop of begin
+  
 }

e8446a0f309ec74deb0b8ca947934279875e0abc
Xuran Cai <xurancai@dmf584.resnet.ust.hk>
2024-03-23 01:42:38 +0800

    generate all permutation with subset

--- src/SDCCralloc2.hpp
+++ src/SDCCralloc2.hpp
@@ -33,22 +33,30 @@ extern "C"
 // Integer constant upper bound on port->num_regs
 #define MAX_NUM_REGS 9
 
+
+typedef std::vector<var_t> f;
+
 //assignment for one instuction
 struct i_assignment_ps{
-   var_t registers[MAX_NUM_REGS][2]; //[0] is the register when begining the instruction, [1] is the register when ending the instruction
+   f registers_begin;
+   f registers_end;
+   varset_t begin_v; //beginning variable
+   varset_t end_v; //ending variable
+
    cfg_node *node; //the corresponding node(with ic) in the cfg
    float cost; //cost of the assignment
 
    i_assignment_ps(){
       for(int i=0; i<MAX_NUM_REGS; i++){
-         registers[i][0] = registers[i][1] = -1;
+         registers_begin[i]=-1;
+         registers_end[i]=-1;
       }
       node = NULL;
    }
 
    bool equal_series(i_assignment_ps *a){
       for(int i=0; i<MAX_NUM_REGS; i++){
-         if(registers[i][1] != a->registers[i][0]){
+         if(registers_end[i] != a->registers_begin[i]){
             return false;
          }
       }
@@ -57,7 +65,7 @@ struct i_assignment_ps{
 
    bool equal(i_assignment_ps *a){
       for(int i=0; i<MAX_NUM_REGS; i++){
-         if(registers[i][0] != a->registers[i][0] || registers[i][1] != a->registers[i][1]){
+         if(registers_end[i] != a->registers_end[i] || registers_begin[i] != a->registers_begin[i]){
             return false;
          }
       }
@@ -69,8 +77,10 @@ struct i_assignment_ps{
 struct assignment_ps{
    float s; //cost
    std::vector<i_assignment_ps> insts; //assignments for each instruction
-   i_assignment_t begin; //beginning register
-   i_assignment_t end; //ending register
+   f begin; //beginning register
+   f end; //ending register
+   varset_t begin_v; //beginning variable
+   varset_t end_v; //ending variable
 };
 
 typedef std::vector<assignment_ps> assignment_ps_list;
@@ -80,8 +90,84 @@ typedef std::vector<assignment_ps> assignment_ps_list;
 //I hope it is not hard, but I am not sure.
 //static float instruction_cost(i_assignment_ps &a);
 
-typedef std::map<std::pair<i_assignment_ps,i_assignment_ps>,assignment_ps> assignment_ps_map;
+typedef std::map<std::pair<f,f>,assignment_ps> assignment_ps_map;
 
+void calcSubset(f& A, std::vector<f >& res,
+                f& subset, int index)
+{
+    // Add the current subset to the result list
+    res.push_back(subset);
+ 
+    // Generate subsets by recursively including and
+    // excluding elements
+    for (int i = index; i < A.size(); i++) {
+        // Include the current element in the subset
+        subset.push_back(A[i]);
+ 
+        // Recursively generate subsets with the current
+        // element included
+        calcSubset(A, res, subset, i + 1);
+ 
+        // Exclude the current element from the subset
+        // (backtracking)
+        subset.pop_back();
+    }
+}
+
+static std::vector<f> generate_permutation(f variables){
+   std::vector<f> results;
+   results.push_back(variables);
+   while (std::next_permutation(variables.begin(), variables.end())){
+      results.push_back(variables);
+   }
+   
+   return results;
+}
+ 
+std::vector<f > subsets(f& A)
+{
+   f subset;
+    std::vector<f > res;
+    int index = 0;
+    calcSubset(A, res, subset, index);
+    return res;
+}
+
+static std::vector<f> generate_possibility(f variables){
+   std::vector<f> results;
+   std::vector<f> sub_set=subsets(variables);
+   for(auto sub:sub_set){
+      if(sub.size()<=MAX_NUM_REGS){
+        f v;
+        for(int i=0; i<MAX_NUM_REGS;i++){
+          v.push_back(-1);
+        }
+        int len=sub.size();
+        for(int i=0;i<len;i++){
+          v[MAX_NUM_REGS-len+i]=sub[i];
+        }
+        cout<<"current sub:"<<endl;
+        for(auto i:v){
+          cout<<i<<" ";
+        }
+        cout<<endl;
+        vector<f> p=generate_permutation(v);
+        cout<<"current p:"<<endl;
+        for(auto i:p){
+          for(auto j:i){
+            cout<<j<<" ";
+          }
+          cout<<endl;
+        }
+        cout<<endl;
+        results.reserve(results.size() + distance(p.begin(),p.end()));
+        results.insert(results.end(),p.begin(),p.end());
+      }
+   }
+   return results;
+}
+
+//---------todo: modified from here----------------
 //this function is used to combine two assignment_ps_list while series merge
 staic combine_assignment_ps_list_series(assignment_ps_list *a, assignment_ps_list *b){
    assignment_ps_list c;
@@ -148,7 +234,6 @@ static combine_assignment_ps_list_loop(assignment_ps_list *a, assignment_ps_list
             ac.s = aa.s + ab.s;
             ac.insts = aa.insts;
             ac.insts.expand(ab.insts);
-            //c.push_back(ac);
             if(cc==NULL || cc.s > ac.s){
                m[std::pair<i_assignment_ps,i_assignment_ps>(aa.begin,aa.end)] = ac;
             }

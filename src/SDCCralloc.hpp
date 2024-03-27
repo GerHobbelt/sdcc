e4eaf59f6624067d66ac03130ad4cc0cef691ea7
Xuran Cai <xurancai@dmf584.resnet.ust.hk>
2024-03-28 02:14:45 +0800

    a

--- src/SDCCralloc.hpp
+++ src/SDCCralloc.hpp
@@ -1295,8 +1295,9 @@ static void dump_cfg(const cfg_t &cfg)
       for (v2 = cfg[i].dying.begin(); v2 != cfg[i].dying.end(); ++v2)
         os << *v2 << " ";
       os<<"stack: ";
-      for (v2 = cfg[i].stack.begin(); v2 != cfg[i].stack.end(); ++v2)
-        os << *v2 << " ";
+      std::set<unsigned int>::const_iterator v3;
+      for (v3 = cfg[i].stack_alive.begin(); v3 != cfg[i].stack_alive.end(); ++v3)
+        os << *v3 << " ";
       name[i] = os.str();
     }
 

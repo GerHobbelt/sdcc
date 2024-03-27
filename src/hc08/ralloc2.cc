071b2e8b3397370d20b8d507ae6a9656fbad6366
Xuran Cai <xurancai@dmf584.resnet.ust.hk>
2024-03-27 22:44:39 +0800

    a

--- src/hc08/ralloc2.cc
+++ src/hc08/ralloc2.cc
@@ -504,9 +504,16 @@ static float instruction_cost(const assignment &a, unsigned short int i, const G
     case SWAP:
       assign_operands_for_cost(a, i, G, I);
       set_surviving_regs(a, i, G, I);
-      c = dryhc08iCode(ic);
-      ic->generated = false;
-      return(c);
+      try{
+        c = dryhc08iCode(ic);
+        ic->generated = false;
+        return(c);
+      }
+      catch(std::exception& e)
+      {
+        ic->generated = fase;
+        return(std::numeric_limits<float>::infinity());
+      }
     default:
       return(0.0f);
     }

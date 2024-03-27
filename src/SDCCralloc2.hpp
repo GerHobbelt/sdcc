d028eeefcdebf42171d305c922fdb250fe4a7b4e
Xuran Cai <xurancai@dmf584.resnet.ust.hk>
2024-03-27 17:19:34 +0800

    a

--- src/SDCCralloc2.hpp
+++ src/SDCCralloc2.hpp
@@ -214,6 +214,7 @@ static void initlize_assignment_ps_list(ps_cfg_t &a, I_t &I){
       for(auto j:end){
          std::cout<<"begin to get cost"<<std::endl;
          assignment_ps aa=assignment_ps();
+         std::cout<<"finish initial assignment_ps"<<std::endl;
          i_assignment_ps as=i_assignment_ps();
           std::cout<<"finish initial assignment"<<std::endl;
          as.registers_begin = i;

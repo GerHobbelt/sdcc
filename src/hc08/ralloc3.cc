952fae756ca8cbe9e8e57dffd6077770c4e588ab
Xuran Cai <xurancai@dmf584.resnet.ust.hk>
2024-03-28 18:23:07 +0800

    debug

--- src/hc08/ralloc3.cc
+++ src/hc08/ralloc3.cc
@@ -577,7 +577,9 @@ static float instruction_cost_easy(const i_assignment_ps &ia,cfg_node &node, con
   float c=0;
 
   wassert (TARGET_IS_HC08 || TARGET_IS_S08);
-
+  if ic->generated
+    return(0.0f);
+    
   switch(ic->op)
     {
     // Register assignment doesn't matter for these:

8f2acf8f7377e6bc8e6e484cecbee86647e44e20
Xuran Cai <xurancai@dmf584.resnet.ust.hk>
2024-03-29 17:32:56 +0800

    add time check

--- src/SDCCralloc2.hpp
+++ src/SDCCralloc2.hpp
@@ -188,13 +188,13 @@ static assignment_ps_map combine_assignment_ps_list_series(ps_cfg_t a, ps_cfg_t 
    assignment_ps_map c;
    assignment_ps_map a_map=a.assignments;
    assignment_ps_map b_map=b.assignments;
-//  auto start = std::chrono::high_resolution_clock::now();
+  auto start = std::chrono::high_resolution_clock::now();
    std::vector<f> begin=generate_possibility(a.begin_v);
    std::vector<f> mid=generate_possibility(a.end_v);
    std::vector<f> end=generate_possibility(b.end_v);
- //  auto stop = std::chrono::high_resolution_clock::now();
-//  auto duration = std::chrono::duration_cast< std::chrono::microseconds>(stop - start);
- //  duration_of_per=duration_of_per+duration.count();
+   auto stop = std::chrono::high_resolution_clock::now();
+  auto duration = std::chrono::duration_cast< std::chrono::microseconds>(stop - start);
+   duration_of_permutation=duration_of_permutation+duration.count();
   // std::cout<<"begin size series:"<<begin.size()<<std::endl;
   // std::cout<<"mid size series:"<<mid.size()<<std::endl;
    for(auto i:begin){
@@ -230,12 +230,12 @@ static assignment_ps_map combine_assignment_ps_list_parallel(ps_cfg_t a, ps_cfg_
    assignment_ps_map c;
    assignment_ps_map a_map=a.assignments;
    assignment_ps_map b_map=b.assignments;
- //  auto start = std::chrono::high_resolution_clock::now();
+   auto start = std::chrono::high_resolution_clock::now();
    std::vector<f> begin=generate_possibility(a.begin_v);
    std::vector<f> end=generate_possibility(b.end_v);
-  // auto stop = std::chrono::high_resolution_clock::now();
-  // auto duration = std::chrono::duration_cast< std::chrono::microseconds>(stop - start);
-  // duration_of_per=duration_of_per+duration.count();
+   auto stop = std::chrono::high_resolution_clock::now();
+   auto duration = std::chrono::duration_cast< std::chrono::microseconds>(stop - start);
+   duration_of_permutation=duration_of_permutation+duration.count();
   // std::cout<<"begin size parallel:"<<begin.size()<<std::endl;
  //  std::cout<<"end size parallel:"<<end.size()<<std::endl;
    for(auto i:begin){
@@ -268,12 +268,12 @@ static assignment_ps_map combine_assignment_ps_list_loop(ps_cfg_t a, ps_cfg_t b)
    assignment_ps_map c;
    assignment_ps_map a_map=a.assignments;
    assignment_ps_map b_map=b.assignments;
-  // auto start = std::chrono::high_resolution_clock::now();
+   auto start = std::chrono::high_resolution_clock::now();
    std::vector<f> end=generate_possibility(a.begin_v);
    std::vector<f> begin=generate_possibility(a.end_v);
-  // auto stop = std::chrono::high_resolution_clock::now();
-  // auto duration = std::chrono::duration_cast< std::chrono::microseconds>(stop - start);
-  // duration_of_per=duration_of_per+duration.count();
+   auto stop = std::chrono::high_resolution_clock::now();
+   auto duration = std::chrono::duration_cast< std::chrono::microseconds>(stop - start);
+   duration_of_permutation=duration_of_permutation+duration.count();
  //   std::cout<<"begin size loop:"<<begin.size()<<std::endl;
  //  std::cout<<"end size loop:"<<end.size()<<std::endl;
    for(auto i:begin){
@@ -325,6 +325,7 @@ static void initlize_assignment_ps_list(ps_cfg_t &a, I_t &I){
          as.registers_end = j;
          //std::cout<<"try to get node"<<std::endl;
          as.node=&((*(a.cfg))[a.begin]);
+         as.global_regs.reserve(a.begin_v.size();
          for(auto i : a.begin_v){
             as.global_regs[i]=getIndex(as.registers_begin,i);
          }

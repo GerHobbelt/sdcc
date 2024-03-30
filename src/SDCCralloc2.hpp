690455468cf9e49c3533da9931f75c268835f39c
Xuran Cai <xurancai@dmf584.resnet.ust.hk>
2024-03-30 23:04:20 +0800

    add

--- src/SDCCralloc2.hpp
+++ src/SDCCralloc2.hpp
@@ -261,7 +261,7 @@ static assignment_ps_map combine_assignment_ps_list_loop(ps_cfg_t a, ps_cfg_t b)
       if(c[newf].s > ac.s){
          ac.begin_cost=i.second.begin_cost;
          ac.end_cost=i.second.end_cost;
-         c[newf] = ac;
+      c[newf] = ac;
    }
    }
    }
@@ -289,8 +289,9 @@ static void initlize_assignment_ps_list(ps_cfg_t &a, I_t &I){
          //std::cout<<"try to get node"<<std::endl;
          as.node=&((*(a.cfg))[a.begin]);
          if (a.begin_v.size()!=0){
+            as.global_regs.reserve(*(a.begin_v.end()-1)+1);
             for(int j=0;j<=*(a.begin_v.end()-1);j++){
-               as.global_regs.push_back(getIndex(i,j));
+               as.global_regs[j]=getIndex(i,j);
             }
          }
          //std::cout<<"try to get cost"<<std::endl;

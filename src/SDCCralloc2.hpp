5b08f446697665b49fd838a3141232c960cb0b24
Xuran Cai <xurancai@dmf584.resnet.ust.hk>
2024-04-02 01:19:12 +0800

    a

--- src/SDCCralloc2.hpp
+++ src/SDCCralloc2.hpp
@@ -147,20 +147,9 @@ static std::vector<f> generate_permutation(f variables){
    std::vector<f> results;
    results.push_back(variables);
    while (std::next_permutation(variables.begin(), variables.end())){
-      bool if_e=true;
-      int last_min=-1;
-      for(int i=0;i<MAX_NUM_REGS;++i){
-         if(variables[i]!=-1){
-            if(variables[i]<last_min){
-               if_e=false;
-               break;}else{
-               last_min=variables[i];
-               }
-         }
-      }
-      if (if_e){
+
       results.push_back(variables);}
-   }
+   
    
    return results;
 }

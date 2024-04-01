e94834b6691895ed65f00b523c433b7678bcc9ec
Xuran Cai <xurancai@dmf584.resnet.ust.hk>
2024-04-02 03:40:36 +0800

    a

--- src/SDCCralloc2.hpp
+++ src/SDCCralloc2.hpp
@@ -205,8 +205,7 @@ static void combine_assignment_ps_list_series(assignment_ps_map &a, assignment_p
    if (va==vb){for (auto i:permutation_map[va]){
      float s=a[i].s+b[i].s;
      c[i]=assignment_ps(s,a[i].begin_cost,b[i].end_cost,va);
-     return;
-   }
+   } return;}
    f v=unionVectors(va,vb);
    std::vector<f> gs; 
    bool newv= (permutation_map.find(v)==permutation_map.end());   
@@ -235,12 +234,12 @@ static void combine_assignment_ps_list_series(assignment_ps_map &a, assignment_p
 
 static void combine_assignment_ps_list_parallel(assignment_ps_map &a, assignment_ps_map &b, assignment_ps_map &c){
 f va=a.begin()->second.variables;
-   f vb=b.begin()->second.variables;
+f vb=b.begin()->second.variables;
    if (va==vb){for (auto i:permutation_map[va]){
      float s=a[i].s+b[i].s-a[i].end_cost-a[i].begin_cost;
      c[i]=assignment_ps(s,a[i].begin_cost,a[i].end_cost,va);
-     return;
-   }}
+   }     return;
+}
    f v=unionVectors(va,vb);
    std::vector<f> gs; 
    bool newv= (permutation_map.find(v)==permutation_map.end());    
@@ -274,8 +273,8 @@ static void combine_assignment_ps_list_loop(assignment_ps_map &a, assignment_ps_
    if (va==vb){for (auto i:permutation_map[va]){
      float s=a[i].s+b[i].s;
      c[i]=assignment_ps(s,b[i].begin_cost,b[i].end_cost,va);
-     return;
-   }}
+   }     return;
+}
 f v=unionVectors(va,vb);
 std::vector<f> gs; 
    bool newv= (permutation_map.find(v)==permutation_map.end());   

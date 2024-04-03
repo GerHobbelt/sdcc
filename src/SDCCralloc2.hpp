7ab12a83060c7fc0aef20987824c3e3b228635d4
Xuran Cai <xurancai@dmf584.resnet.ust.hk>
2024-04-03 21:53:12 +0800

    update

--- src/SDCCralloc2.hpp
+++ src/SDCCralloc2.hpp
@@ -380,7 +380,7 @@ static void initlize_assignment_ps_list(ps_cfg_t &a, I_t &I){
     
    std::vector<f>  begin_p=generate_p_w(a.begin_v);
 
-   cfg_node node=cfg_map[a.index];
+   cfg_node node=cfg_map[a.index][0];
    int n = boost::num_vertices(I);
    for(auto i:begin_p){
          f global;

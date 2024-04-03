1e5077cc446e5393b7b75e7258e5ca9bedee861d
Xuran Cai <xurancai@dmf584.resnet.ust.hk>
2024-04-04 03:08:43 +0800

    update

--- src/SDCCralloc2.hpp
+++ src/SDCCralloc2.hpp
@@ -392,8 +392,8 @@ static void initlize_assignment_ps_list(ps_cfg_t &a, I_t &I){
 }
 
 
-template <class I_t>
-static void generate_spcfg(ps_cfg_t &ps_cfg, I_t &I){
+
+static void generate_spcfg(ps_cfg_t &ps_cfg){
  // if (ps_cfg.make_series){
   //    series_addition(ps_cfg);
    //   return;
@@ -401,19 +401,13 @@ static void generate_spcfg(ps_cfg_t &ps_cfg, I_t &I){
 
    if (ps_cfg.assignments.size() == 0){
 
-       if(ps_cfg.left==-1 || ps_cfg.right==-1){
-        // std::cout<<"1"<<std::endl;
-         initlize_assignment_ps_list(ps_cfg, I);
-         return;
-      }
-
       if (ps_cfg_map[ps_cfg.left].assignments.size() == 0){
        //  std::cout<<"2"<<std::endl;
-         generate_spcfg(ps_cfg_map[ps_cfg.left],I);
+         generate_spcfg(ps_cfg_map[ps_cfg.left]);
       }
       if (ps_cfg_map[ps_cfg.right].assignments.size() == 0){
        //  std::cout<<"3"<<std::endl;
-         generate_spcfg(ps_cfg_map[ps_cfg.right],I);
+         generate_spcfg(ps_cfg_map[ps_cfg.right]);
       }
       switch (ps_cfg.type){
          case 1:

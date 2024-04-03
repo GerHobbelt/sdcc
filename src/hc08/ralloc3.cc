d5677afb8e61e9c473f061008c9178c677bd19cb
Xuran Cai <xurancai@dmf584.resnet.ust.hk>
2024-04-03 21:51:58 +0800

    update

--- src/hc08/ralloc3.cc
+++ src/hc08/ralloc3.cc
@@ -382,7 +382,7 @@ float hc08_ralloc3_cc(ebbIndex *ebbi)
   boost::graph_traits<cfg_t>::vertex_iterator vi, vi_end;
   boost::tie(vi, vi_end) = boost::vertices(control_flow_graph);
   cfg_map.push_back(control_flow_graph);
-  root=init_ps_cfg(control_flow_graph,*vi,*(vi_end-1),0,-1);
+  root=init_ps_cfg(*vi,*(vi_end-1),0,-1);
   ps_cfg_map.push_back(root);
   std::cout<<"root created"<<std::endl;
   try{

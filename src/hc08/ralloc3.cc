5d26f1a6312ceef5910034dd4fad46311f31ac82
Xuran Cai <xurancai@dmf584.resnet.ust.hk>
2024-04-04 00:25:58 +0800

    update

--- src/hc08/ralloc3.cc
+++ src/hc08/ralloc3.cc
@@ -392,12 +392,12 @@ float hc08_ralloc3_cc(ebbIndex *ebbi)
 
   std::cout<<"spcfg created"<<std::endl;
   float cost= get_ps_optimal_cst(root,conflict_graph);
+  return  cost;
   //std::cout<<"get cost"<<std::endl;
 }catch(std::exception &e){
     std::cout<<e.what()<<std::endl;
    return -1;
   }
 
-  return  cost;
 }
 
